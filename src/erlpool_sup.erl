3051ddd709c6376ccb9caac4b90ce469463688da
Led <ledest@gmail.com>
2017-06-18 04:30:32 +0300

    erlpool_sup: formatting; inline supervisor_childspec/4

--- src/erlpool_sup.erl
+++ src/erlpool_sup.erl
@@ -2,30 +2,25 @@
 
 -behaviour(supervisor).
 
--export([
-    start_link/0,
-    add_pool/2,
-    remove_pool/1,
-    init/1
-]).
+-export([start_link/0,
+         add_pool/2,
+         remove_pool/1,
+         init/1]).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 add_pool(PoolName, PoolArgs) ->
-    ChildSpecs = supervisor_childspec(PoolName, erlpool_pool_sup, infinity, [PoolName,  PoolArgs]),
-    supervisor:start_child(?MODULE, ChildSpecs).
+    supervisor:start_child(?MODULE, supervisor_childspec(PoolName, erlpool_pool_sup, infinity, [PoolName,  PoolArgs])).
 
 remove_pool(PoolName) ->
     case supervisor:terminate_child(?MODULE, PoolName) of
-        ok ->
-            supervisor:delete_child(?MODULE, PoolName);
-        Error ->
-            Error
+        ok -> supervisor:delete_child(?MODULE, PoolName);
+        Error -> Error
     end.
 
-init([]) ->
-    {ok, { {one_for_one, 1000, 1}, []} }.
+init([]) -> {ok, {{one_for_one, 1000, 1}, []}}.
 
 supervisor_childspec(Name, Module, WaitForClose, Args) ->
     {Name, {Module, start_link, Args}, transient, WaitForClose, supervisor, [Module]}.
+
+-compile({inline, [supervisor_childspec/4]}).
