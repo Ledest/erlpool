442b096c7c80e824ce3a43d4df5d380f6a5cf7bb
Led <ledest@gmail.com>
2017-06-18 04:33:44 +0300

    erlpool_globals: formatting; clean up code

--- src/erlpool_globals.erl
+++ src/erlpool_globals.erl
@@ -6,9 +6,6 @@
 %% The only purpose for the module is to suppress warnings from code analyzers,
 %% as dynamically compiled module is not available during the build.
 
--export([]).
-
 -on_load(do_not_load/0).
 
-do_not_load() ->
-    do_not_load.
+do_not_load() -> do_not_load.
