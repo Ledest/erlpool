42d24b91045cfe914ae7540895ac1bf6ed873de7
Led <ledest@gmail.com>
2017-06-18 04:31:52 +0300

    erlpool_pul_sup: formatting; shrink code; inline children_specs/3
    Add defines from erlpool.hrl

--- src/erlpool_pool_sup.erl
+++ src/erlpool_pool_sup.erl
@@ -2,49 +2,41 @@
 
 -author("silviu.caragea").
 
--include("erlpool.hrl").
+-define(DEFAULT_MAX_PERIOD_SEC, 1).
+-define(DEFAULT_MAX_INTENSITY, 100).
+-define(DEFAULT_SUP_RESTART_STRATEGY, permanent).
 
 -behaviour(supervisor).
 
--define(SUPERVISOR_NAME(PoolName), list_to_atom(atom_to_list(?MODULE) ++ "_" ++ atom_to_list(PoolName) ++ "_sup")).
-
--export([
-    start_link/2,
-    init/1,
-    start_worker/3,
-    name/1
-]).
+-export([start_link/2,
+         init/1,
+         start_worker/3,
+         name/1]).
 
 start_link(PoolName, PoolArgs) ->
-    supervisor:start_link({local, ?SUPERVISOR_NAME(PoolName)}, ?MODULE, [PoolName, PoolArgs]).
+    supervisor:start_link({local, name(PoolName)}, ?MODULE, [PoolName, PoolArgs]).
 
-name(PoolName) ->
-    ?SUPERVISOR_NAME(PoolName).
+name(PoolName) -> list_to_atom(lists:concat([?MODULE_STRING "_", PoolName, "_sup"])).
 
 %internals
 
 init([PoolName, PoolArgs]) ->
-    PoolSize = proplists:get_value(size, PoolArgs),
-    SupRestartPeriod = proplists:get_value(supervisor_period, PoolArgs, ?DEFAULT_MAX_PERIOD_SEC),
-    SupIntensity = proplists:get_value(supervisor_intensity, PoolArgs, ?DEFAULT_MAX_INTENSITY),
     SupRestartStrategy = proplists:get_value(supervisor_restart, PoolArgs, ?DEFAULT_SUP_RESTART_STRATEGY),
     MFA = proplists:get_value(start_mfa, PoolArgs),
-
     PoolTable = ets:new(PoolName, [named_table, public, set, {write_concurrency, true}, {read_concurrency, true}]),
     true = ets:insert(PoolTable, [{sq, 0}]),
-
-    CreateFun = fun(Id) ->
-        children_specs(Id, SupRestartStrategy, [Id, PoolTable, MFA])
-    end,
-
-    Ch = lists:map(CreateFun, lists:seq(1, PoolSize)),
-
-    {ok, {{one_for_one, SupIntensity, SupRestartPeriod}, Ch}}.
+    {ok,
+     {{one_for_one,
+       proplists:get_value(supervisor_intensity, PoolArgs, ?DEFAULT_MAX_INTENSITY),
+       proplists:get_value(supervisor_period, PoolArgs, ?DEFAULT_MAX_PERIOD_SEC)},
+      [children_specs(Id, SupRestartStrategy, [Id, PoolTable, MFA]) || Id <- lists:seq(1, proplists:get_value(size, PoolArgs))]}}.
 
 start_worker(Id, PoolTable, {M, F, A}) ->
-    {ok, Pid} = erlang:apply(M, F, A),
+    {ok, Pid} = R = apply(M, F, A),
     true = ets:insert(PoolTable, {Id, Pid}),
-    {ok, Pid}.
+    R.
 
 children_specs(Name, SupRestartStrategy, Args) ->
     {Name, {?MODULE, start_worker, Args}, SupRestartStrategy, 2000, worker, [?MODULE]}.
+
+-compile({inline, [children_specs/3]}).
